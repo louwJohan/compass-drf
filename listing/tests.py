0c9c69b60e9384c11b47123d1b6945a501dac439
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-09-27 15:57:36 +0000

    Add tests to listings

--- listing/tests.py
+++ listing/tests.py
@@ -1,3 +1,86 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Listing
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ListingListTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='ben', password='pass')
+
+    def test_can_list_posts(self):
+        ben = User.objects.get(username='ben')
+        Listing.objects.create(owner=ben, title='Test')
+        response = self.client.get('/listings/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
+
+    def test_logged_in_user_can_create_listing(self):
+        self.client.login(username='ben', password='pass')
+        response = self.client.post('/listings/', {'title': 'test',
+                                                   'description': 'house',
+                                                   'type_of_property': 'detached_house',
+                                                   'bedrooms': '4',
+                                                   'area': 'Leeds',
+                                                   'price': '200000',
+                                                   'commerce_type': 'sell',
+                                                   'image_one': '',
+                                                   'image_two': '',
+                                                   'image_three': '',
+                                                   'image_four': '',
+                                                   'image_five': '',
+                                                   'image_six': '',
+                                                   'image_seven': '',
+                                                   'image_eight': ''
+                                                   })
+        count = Listing.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_user_not_logged_in_cannot_create_listing(self):
+        response = self.client.post('/listings/', {'title': 'test',
+                                                   
+                                                   })
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class ListingDetailViewTests(APITestCase):
+    def setUp(self):
+        ben = User.objects.create_user(username='ben', password='pass')
+        peter = User.objects.create_user(username='peter', password='word')
+        Listing.objects.create(
+            owner=ben, title='test1', description='house',
+            type_of_property='detached_house', bedrooms='4',
+            area='Leeds', price='200000', commerce_type='sell',
+            image_one='', image_two='', image_three='', image_four='',
+            image_five='', image_six='', image_seven='', image_eight=''
+        )
+        Listing.objects.create(
+            owner=ben, title='test2', description='house2',
+            type_of_property='detached_house', bedrooms='5',
+            area='Leeds', price='2000000', commerce_type='sell',
+            image_one='', image_two='', image_three='', image_four='',
+            image_five='', image_six='', image_seven='', image_eight=''
+        )
+
+    def test_can_retrieve_listing_using_valid_id(self):
+        response = self.client.get('/posts/1/')
+        self.assertEqual(response.data['title'], 'test1')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+#     def test_can_retrieve_post_using_invalid_id(self):
+#         response = self.client.get('/posts/100/')
+#         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+#     def test_user_can_update_own_post(self):
+#         self.client.login(username='ben', password='pass')
+#         response = self.client.put('/posts/1/', {'title': 'new title'})
+#         post = Post.objects.filter(pk=1).first()
+#         self.assertEqual(post.title, 'new title')
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+#     def test_user_can_update_another_users_post(self):
+#         self.client.login(username='peter', password='word')
+#         response = self.client.put('/posts/1/', {'title': 'new title'})
+#         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
