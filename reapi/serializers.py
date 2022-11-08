5dccdd61b6066634744ced7cd128a4ea304f68dd
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-11-08 12:08:46 +0000

    Fix bug in serializer

--- reapi/serializers.py
+++ reapi/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 
 
 class CurrentUserSerializer(UserDetailsSerializer):
-    profile_id = serializers.ReadOnlyField(source='profile.id')
+    profile_id = serializers.ReadOnlyField(source='profile_id')
     profile_image = serializers.ReadOnlyField(source='profile.profile_image.url')
 
     class Meta(UserDetailsSerializer.Meta):
